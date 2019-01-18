df80ed16c3436c22e9bb716fa48349263ae8551e
Umang J Gala <toumangg@gmail.com>
2019-01-17 20:05:39 -0500

    test for NextGreaterNumber problem

---
+++ src/test/java/org/svaarg/problemsolving/TestHelper.java
@@ -0,0 +1,20 @@
+package org.svaarg.problemsolving;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class TestHelper {
+  public static Collection<Object[]> getTestCases(File directory) {
+    Collection<Object[]> testCases = new ArrayList<>();
+    for (File file : directory.listFiles()) {
+      if (file.getName().endsWith(".in")) {
+        testCases.add(new Object[] {
+                new File(file.getAbsolutePath()),
+                new File(file.getAbsolutePath().replace(".in",".out"))
+        });
+      }
+    }
+    return testCases;
+  }
+}
---
+++ src/test/java/org/svaarg/problemsolving/array/NextGreaterNumberTest.java
@@ -0,0 +1,56 @@
+package org.svaarg.problemsolving.array;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.svaarg.problemsolving.TestHelper;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Class NextGreaterNumberTest
+ *
+ * @author Umang J Gala
+ */
+@RunWith(Parameterized.class)
+public class NextGreaterNumberTest {
+  @Parameterized.Parameters(name = "array/NextGreaterNumberTest - Test: {index}")
+  public static Collection<Object[]> data() {
+    String directoryPath = System.getProperty("testFilesRootDirectory");
+    directoryPath += "array/nextgreaternumber/";
+    return TestHelper.getTestCases(new File(directoryPath));
+  }
+
+  @Parameter()
+  public File inputFile;
+
+  @Parameter(1)
+  public File outputFile;
+
+  private Scanner input, output;
+
+  @org.junit.Before
+  public void setUp() throws Exception {
+    input = new Scanner(inputFile);
+    output = new Scanner(outputFile);
+  }
+
+  @org.junit.Test
+  public void nextGreaterNumberTest() {
+      Integer number = input.nextInt();
+      int expectedOutput = output.nextInt();
+      NextGreaterNumber object = new NextGreaterNumber();
+      assertEquals(expectedOutput, object.findNext(number.toString().toCharArray()));
+  }
+
+  @org.junit.After
+  public void tearDown() {
+    input.close();
+    output.close();
+  }
+}
