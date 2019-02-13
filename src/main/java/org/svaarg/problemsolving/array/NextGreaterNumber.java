417f38783fdb01d87593191fa4642c79a4b6d327
Umang J Gala <toumangg@gmail.com>
2019-02-12 19:51:45 -0500

    problem statement description for NextGreaterNumber problem

--- src/main/java/org/svaarg/problemsolving/array/NextGreaterNumber.java
+++ src/main/java/org/svaarg/problemsolving/array/NextGreaterNumber.java
@@ -1,5 +1,35 @@
 package org.svaarg.problemsolving.array;
 
+/*
+Problem Statement
+-----------------
+Given a number n, find the smallest number that has same set of digits as n
+and is greater than n. If x is the greatest possible number with its set of
+digits, then print "NOT POSSIBLE"
+
+Input Format
+------------
+Input contains only one integer value n
+
+Output Format
+-------------
+The output contains single line a number, smallest and greater than n with same
+set of digits or -1 if no such number possible
+
+Sample Input 1:
+218765
+
+Sample Output 1:
+251678
+
+Sample Input 2:
+4321
+
+Sample Output 2:
+-1
+
+ */
+
 import org.svaarg.problemsolving.util.Utility;
 
 import java.util.Arrays;
