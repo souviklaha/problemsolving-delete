8444949c64543c3bdc14d4e4a87c3da8b5eac507
Umang J Gala <toumangg@gmail.com>
2019-05-10 21:01:35 -0400

    #10 Problem: Sorting Algorithms

---
+++ src/main/java/org/svaarg/problemsolving/sort/QuickSort.java
@@ -0,0 +1,34 @@
+package org.svaarg.problemsolving.sort;
+
+import org.svaarg.problemsolving.util.Utility;
+
+/**
+ * Class: QuickSort
+ *
+ * @author Umang J Gala
+ */
+public class QuickSort {
+  public static void sort(int[] elements, int start, int end) {
+    if (start < end) {
+      int partition = partition(elements, start, end);
+      sort(elements, start, partition - 1);
+      sort(elements, partition + 1, end);
+    }
+  }
+
+  private static int partition(int[] elements, int start, int end) {
+    int startIndex = start;
+    int endIndex = end;
+    int startElement = elements[start];
+    while (startIndex < endIndex) {
+      while (startIndex <= end && elements[startIndex] <= startElement)
+        startIndex++;
+      while (elements[endIndex] > startElement)
+        endIndex--;
+      if (startIndex < endIndex)
+        Utility.swapArrayElements(elements, startIndex, endIndex);
+    }
+    Utility.swapArrayElements(elements, start, endIndex);
+    return endIndex;
+  }
+}
---
+++ src/main/java/org/svaarg/problemsolving/sort/SelectionSort.java
@@ -0,0 +1,25 @@
+package org.svaarg.problemsolving.sort;
+
+import org.svaarg.problemsolving.util.Utility;
+
+/**
+ * Class: SelectionSort
+ *
+ * @author Umang J Gala
+ */
+public class SelectionSort {
+  public static void sort(int[] elements) {
+    int minimum, minimumIndex;
+    for (int element = 0; element < elements.length - 1; element++) {
+      minimum = elements[element];
+      minimumIndex = element;
+      for (int index = element + 1; index < elements.length; index++) {
+        if (elements[index] < minimum) {
+          minimum = elements[index];
+          minimumIndex = index;
+        }
+      }
+      Utility.swapArrayElements(elements, element, minimumIndex);
+    }
+  }
+}
