8f2c254d43e528ae2e9fe46b7beb21d304b1e8e4
Umang G <ujg2794@rit.edu>
2020-05-07 12:43:47 -0400

    Two Sum problem
    Add three different solution, refactor test using Junit 5 and delete unwanted files

---
+++ src/test/java/org/swaarg/problemsolving/leetcode/TwoSumTest.java
@@ -0,0 +1,42 @@
+package org.swaarg.problemsolving.leetcode;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * TwoSumTest
+ *
+ * @author Umang G
+ */
+class TwoSumTest {
+  private static Stream<Arguments> testCases() {
+    return Stream.of(
+        Arguments.of(new int[]{2, 7, 11, 15}, 9, new int[]{0, 1}),
+        Arguments.of(new int[]{6, 2, 8, 4, 3}, 18, new int[]{-1, -1}),
+        Arguments.of(new int[]{9}, 2, new int[]{-1, -1})
+    );
+  }
+
+  @ParameterizedTest(name="One-pass Hash Table - {index} > {0}, {1} > ")
+  @MethodSource("testCases")
+  public void testTwoSumOnePassHashTable(int[] nums, int target, int[] expected) {
+    assertArrayEquals(expected, TwoSum.twoSumOnePassHashTable(nums, target));
+  }
+
+  @ParameterizedTest(name="Two-pass Hash Table - {index} > {0}, {1} > ")
+  @MethodSource("testCases")
+  public void testTwoSumTwoPassHashTable(int[] nums, int target, int[] expected) {
+    assertArrayEquals(expected, TwoSum.twoSumTwoPassHashTable(nums, target));
+  }
+
+  @ParameterizedTest(name="Brute Force Hash Table - {index} > {0}, {1} > ")
+  @MethodSource("testCases")
+  public void testTwoSumBruteForce(int[] nums, int target, int[] expected) {
+    assertArrayEquals(expected, TwoSum.twoSumBruteForce(nums, target));
+  }
+}
